2ecf36a826385db3bfeee57ea1f3b5503741e285
JCrucillo2 <junnelcrucillo729@outlook.com>
2022-11-12 21:37:10 -0700

    Finished projects section

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,9 @@ import { SiAdobexd } from "react-icons/si";
 // import images
 import jclogo from "../public/img/my-logo.png";
 import junnel from "../public/img/my-picture.webp";
+import BsProject from "../public/img/bootstrap-page.webp";
+import NextJStripe from "../public/img/next-js-stripe-intg.webp";
+import ReactDashboard from "../public/img/react-dashboard.webp";
 
 export default function Home() {
   return (
@@ -37,9 +40,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-white px-4">
+      <main className="bg-white">
         <section className="min-h-screen">
-          <nav className="flex items-center justify-between py-8 mb-12 md:container mx-auto">
+          <nav className="flex items-center justify-between py-8 md:container mx-auto px-4">
             <h1>
               <Link href="/" className="flex items-center">
                 <Image
@@ -68,7 +71,7 @@ export default function Home() {
             </ul>
           </nav>
 
-          <div className="grid md:grid-cols-2 gap-4 mx-auto md:container items-center mb-12">
+          <div className="grid md:grid-cols-2 gap-4 mx-auto md:container items-center mb-12 px-4">
             <div className="md:flex justify-center">
               <Image
                 src={junnel}
@@ -83,17 +86,17 @@ export default function Home() {
               <h3 className="text-2xl py-2 font-medium uppercase">
                 Web Developer
               </h3>
-              <p className="mb-4 text-sm leading-8 text-gray-800">
+              <p className="mb-4 text-sm leading-8 text-gray-600">
                 Hi there! I am a Digital Media and IT (Information Technology)
                 specialized in Web Design and Development who is passionate
                 about making functional and well-designed websites.
               </p>
-              <p className="mb-4 text-sm leading-8 text-gray-800">
+              <p className="mb-4 text-sm leading-8 text-gray-600">
                 With my passion and enthusiasm in the web development world, I
                 can assure people that I always give the best of my abilities to
                 create their websites.
               </p>
-              <p className="mb-4 text-sm leading-8 text-gray-800">
+              <p className="mb-4 text-sm leading-8 text-gray-600">
                 I am always excited to further enhance my skills and take it to
                 the next level. Contact me so that I can showcase what I can do
                 for you!
@@ -105,7 +108,7 @@ export default function Home() {
                   </Link>
                 </li>
                 <li>
-                  <Link href="https://github.com/">
+                  <Link href="https://github.com/JCrucillo2">
                     <AiFillGithub className="text-7xl transition-all hover:fill-teal-500" />
                   </Link>
                 </li>
@@ -118,11 +121,11 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="md:container mx-auto">
+          <div className="md:container mx-auto px-4">
             <h2 className="uppercase font-bold text-4xl text-teal-600 text-center underline">
               Skills
             </h2>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 justify-items-center py-8">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 justify-items-center py-9">
               <div className="flex items-center flex-col">
                 <AiFillHtml5 className="text-8xl transition-all hover:fill-teal-500" />
                 <p className="text-sm font-bold">HTML5</p>
@@ -162,6 +165,144 @@ export default function Home() {
               <div className="flex items-center flex-col">
                 <DiResponsive className="text-8xl transition-all hover:fill-teal-500" />
                 <p className="text-sm font-bold">Responsive Design</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-teal-500">
+            <div className="md:container mx-auto px-4 text-white">
+              <h2 className="uppercase font-bold text-4xl pt-9 text-center underline">
+                Projects
+              </h2>
+              <div className="py-9">
+                <div className="grid lg:grid-cols-2 gap-8 items-center mb-12">
+                  <div>
+                    <Image
+                      src={BsProject}
+                      alt="Bootstrap project"
+                      className="w-fit rounded-md"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="text-3xl font-bold my-4">
+                      Bootstrap Project
+                    </h3>
+                    <p className=" text-gray-800">
+                      A project website designed using a CSS framework called
+                      Bootstrap. With this framework, I learned how to design a
+                      website using custom classes to create a responsive and
+                      well-designed website.
+                    </p>
+                    <p className="my-4">
+                      <span className="font-bold">Tools used:</span> HTML,
+                      Bootstrap 4
+                    </p>
+                    <div className="uppercase flex items-center gap-4">
+                      <Link
+                        href="https://bootstrap-final.netlify.app/"
+                        className="bg-black font-bold text-white p-2.5 rounded-md transition-all hover:bg-white hover:text-black"
+                        target="_blank"
+                      >
+                        Live Site
+                      </Link>
+                      <Link
+                        href="https://github.com/JCrucillo2/a03-Assignment-4"
+                        className="bg-black font-bold text-white p-2.5 rounded-md transition-all hover:bg-white hover:text-black"
+                        target="_blank"
+                      >
+                        GitHub
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="grid lg:grid-cols-2 gap-8 items-center mb-12">
+                  <div className="lg:order-last">
+                    <Image
+                      src={ReactDashboard}
+                      alt="React Dashboard Project"
+                      className="w-fit rounded-md"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="text-3xl font-bold my-4">React Dashboard</h3>
+                    <p className=" text-gray-800">
+                      React dashboard is my very first project I did with React
+                      and I loved it ever since. This site features a functional
+                      login page that protects anything inside it even if
+                      manually redirecting to the main dashboard. It also
+                      includes integrating Firebase Database to perform various
+                      tasks such as reading, creating, editing, and deleting
+                      data. I learned how to use async/await and Firebase api
+                      while working on this project.
+                    </p>
+                    <p className="my-4">
+                      <span className="font-bold">Tools used:</span> React,
+                      Firebase, styled-components, react-icons
+                    </p>
+                    <div className="uppercase flex items-center gap-4">
+                      <Link
+                        href="https://jcfigures-react.netlify.app/"
+                        className="bg-black font-bold text-white p-2.5 rounded-md transition-all hover:bg-white hover:text-black"
+                        target="_blank"
+                      >
+                        Live Site
+                      </Link>
+                      <Link
+                        href="https://github.com/JCrucillo2/jcfigures-react"
+                        className="bg-black font-bold text-white p-2.5 rounded-md transition-all hover:bg-white hover:text-black"
+                        target="_blank"
+                      >
+                        GitHub
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="grid lg:grid-cols-2 gap-8 items-center">
+                  <div>
+                    <Image
+                      src={NextJStripe}
+                      alt="NextJS Project with Stripe"
+                      className="w-fit rounded-md"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="text-3xl font-bold my-4">
+                      NextJS w/ StripeJS Integration
+                    </h3>
+                    <p className=" text-gray-800">
+                      In this project, We used a react framework called Next.js
+                      to create a storefront. Together, along with what I
+                      learned from using Firebase, I was able to create a
+                      dynamic storefront that I can control which products I
+                      want to sell. We also integrated StripeJS as a payment
+                      method so that users are able to checkout items with a
+                      clean UI. Everything in here is designed with Tailwind
+                      CSS.
+                    </p>
+                    <p className="my-4">
+                      <span className="font-bold">Tools used:</span> NextJS,
+                      StripeJS, Firebase, Tailwind
+                    </p>
+                    <div className="uppercase flex items-center gap-4">
+                      <Link
+                        href="https://jcfigures-next.vercel.app/"
+                        className="bg-black font-bold text-white p-2.5 rounded-md transition-all hover:bg-white hover:text-black"
+                        target="_blank"
+                      >
+                        Live Site
+                      </Link>
+                      <Link
+                        href="https://github.com/JCrucillo2/jcfigures-next"
+                        className="bg-black font-bold text-white p-2.5 rounded-md transition-all hover:bg-white hover:text-black"
+                        target="_blank"
+                      >
+                        GitHub
+                      </Link>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
